1480b0d04b9b6be1307d22df2dfd3df6d2092d49
Nodge <76185358+KartWrightfield@users.noreply.github.com>
2024-01-19 17:02:27 +0000

    Finished nicknames prototype

--- NodeApp/index.js
+++ NodeApp/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 
 const Nickname = require('./models/nickname');
 
@@ -17,6 +18,7 @@ mongoose.connect('mongodb://localhost:27017/robotLegs')
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
 
 
 app.get('/nicknames', async (_req, res) => {
@@ -30,7 +32,6 @@ app.get('/nicknames/new', (req, res) => {
 })
 
 app.post('/nicknames', async (req, res) => {
-
     const newNickname = new Nickname(req.body);
     newNickname.nicknames.push(req.body.nickname);
 
@@ -39,11 +40,36 @@ app.post('/nicknames', async (req, res) => {
     res.redirect(`/nicknames/${newNickname.id}`);
 })
 
+app.patch('/nicknames/:id', async (req, res) => {
+    const { id } = req.params;
+    const nickname = req.body.nickname;
+
+    const playerNicknames = await Nickname.findById(id);
+
+    playerNicknames.nicknames.push(nickname);
+
+    await playerNicknames.save();
+
+    res.redirect(`/nicknames/${id}`);
+})
+
+app.delete('/nicknames/:id', async (req, res) => {
+    const { id } = req.params;
+    await Nickname.findByIdAndDelete(id);
+    
+    res.redirect('/nicknames');
+})
+
 app.get('/nicknames/:id', async (req, res) => {
     const { id } = req.params;
     const nickname = await Nickname.findById(id);
 
     res.render('nicknames/details', { nickname });
+})
+
+app.get('/nicknames/:id/addNickname', (req, res) => {
+    const { id } = req.params;
+    res.render(`nicknames/addNickname`, { id });
 })
 
 app.listen(3000, () => {
